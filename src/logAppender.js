721cdf9c07497998be4bfe6edfa819de3e57a978
Richard Simpson <richardsimpson@outlook.com>
2017-04-25 17:56:41 -0500

    Swap enums with hard coded values

--- src/logAppender.js
+++ src/logAppender.js
@@ -4,18 +4,18 @@ import {
 
 export default class AppInsightsAppender {
 	debug(logger, message) {
-		AppInsights.trackTrace(message, { "id": logger.id }, AI.SeverityLevel.Verbose);
+		AppInsights.trackTrace(message, { "id": logger.id }, /* Verbose */ 0);
 	}
 
 	info(logger, message) {
-		AppInsights.trackTrace(message, { "id": logger.id }, AI.SeverityLevel.Information);
+		AppInsights.trackTrace(message, { "id": logger.id }, /* Information */ 1);
 	}
 
 	warn(logger, message) {
-		AppInsights.trackTrace(message, { "id": logger.id }, AI.SeverityLevel.Warning);
+		AppInsights.trackTrace(message, { "id": logger.id }, /* Warning */ 2);
 	}
 
 	error(logger, message) {
-		AppInsights.trackException(message, "framework", { "id": logger.id }, {}, AI.SeverityLevel.Error);
+		AppInsights.trackException(message, "framework", { "id": logger.id }, {}, /* Error */ 3);
 	}
 }
